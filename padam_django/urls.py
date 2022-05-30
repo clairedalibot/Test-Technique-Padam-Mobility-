2a355365e1a7491d8a1c2f0ea193976ff6ce76d0
clairedalibot <clairedalibot@yahoo.fr>
2022-05-30 11:57:56 +0200

    redirection de la vue principale

--- padam_django/urls.py
+++ padam_django/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import redirect_view
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
+    path('', redirect_view),
     path('bs_planner/', include("bs_planner.urls"))
 ]
